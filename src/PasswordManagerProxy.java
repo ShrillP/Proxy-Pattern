4201057fb7ddbf5bd98c999363ecb26aaa360d7e
Shrill Patel <1006shrillpatel@gmail.com>
2023-04-12 04:55:41 -0400

    implemented proxy interface + setup proxy class

--- src/PasswordManagerProxy.java
+++ src/PasswordManagerProxy.java
@@ -1,3 +1,21 @@
-public class PasswordManagerProxy {
-    
+public class PasswordManagerProxy implements PasswordManager {
+    private boolean isPasswordUsed;
+    private Client currentClient;
+    private FIM fim;
+
+    public PasswordManagerProxy(Client currentClient) {
+        this.currentClient = currentClient;
+        this.fim = new FIM();
+        this.isPasswordUsed = false;
+    }
+
+    public void updatePassword() {
+    }
+
+    private boolean verifyPasswordUsed() {
+        return false;
+    }
+
+    private void informClient() {
+    }
 }
