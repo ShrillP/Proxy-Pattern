1fccf86f95b949e21e41dbefb053a94f20808ac1
Shrill Patel <1006shrillpatel@gmail.com>
2023-04-12 04:50:51 -0400

    implemented Client class

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,17 @@
 public class Client {
-    
+    private String userID;
+    private String newPassword;
+
+    public Client(String userID, String newPassword) {
+        this.userID = userID;
+        this.newPassword = newPassword;
+    }
+
+    public String getUserID() {
+        return this.userID;
+    }
+
+    public String getNewPassword() {
+        return this.newPassword;
+    }
 }
